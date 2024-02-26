6421d1675e2e03d2ba7823db12e655dce994c0f7
Mehmet Yıldırım <65573335+erythropygia@users.noreply.github.com>
2024-02-26 16:58:08 +0300

    Update Struckt işlemler.c

--- HomeworkandQuiz/3. Homework/Struckt işlemler.c
+++ HomeworkandQuiz/3. Homework/Struckt işlemler.c
@@ -1,139 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void kelimeislemleri();
-int benzerlikhesapla(char girilenkelime[10],int uzunluk);
-void chardizisiatama();
-static int indis=1;
 
-struct kelimehesap{	
-    char liste[15][15];
-	int sesli;
-	int sessiz;
-	int uzunluk;
-	int benzerlik;
-}kelime;
+void wordOperations();
+int calculateSimilarity(char enteredWord[10], int length);
+void initializeCharArray();
+static int index = 1;
 
+struct WordAnalysis {
+    char list[15][15];
+    int vowels;
+    int consonants;
+    int length;
+    int similarity;
+} word;
 
-int main(){
-	
-	chardizisiatama();
-	kelimeislemleri();
-	int i,sonuc,enbuyuk=0;
-    char girilenkelime[10];
-	printf("Dizideki Stringlerle Karsilastirma Icin Kelime Giriniz:");
-	scanf("%s",&girilenkelime);
-	for(i=0;i<15;i++){
-		kelime.benzerlik=benzerlikhesapla(girilenkelime, i);
-		if(enbuyuk<kelime.benzerlik){
-			enbuyuk=kelime.benzerlik;
-			sonuc=i;	
-		}
-	}
-	printf("En Fazla Benzesme %s Kelimesi, Kelime %d. Indistedir \n ",kelime.liste[sonuc],sonuc+1);
-	return 0;
+int main() {
+    initializeCharArray();
+    wordOperations();
+    int i, result, maxSimilarity = 0, maxIndex = 0;
+    char enteredWord[10];
+
+    printf("Enter a word for comparison with strings in the array:");
+    scanf("%s", enteredWord);
+
+    for (i = 0; i < 15; i++) {
+        word.similarity = calculateSimilarity(enteredWord, i);
+        if (maxSimilarity < word.similarity) {
+            maxSimilarity = word.similarity;
+            maxIndex = i;
+        }
+    }
+
+    printf("The word with the highest similarity is %s at index %d.\n", word.list[maxIndex], maxIndex + 1);
+
+    return 0;
 }
 
+int calculateSimilarity(char enteredWord[10], int length) {
+    int condition3 = 0, condition2 = 0, condition1 = 0, totalScore = 0;
 
-int benzerlikhesapla(char girilenkelime[10],int uzunluk){
-	int kosul3=0,kosul2=0,kosul1=0,toplampuan=0;
-	
-	int i,j;
-	for (i=0; i<15; i++){
-		if(kelime.liste[uzunluk][i]=='\0'){
-			break;
-		}
-	}
-	int uzunluk1=i;	
-	for(i=0; i<15;i++){
-		if(girilenkelime[i]=='\0'){
-			break;
-		}
-	}
-	int uzunluk2=i;
-	
-	for (i=0; i<uzunluk1; i++){
-		for(j=0; j<uzunluk2;j++){
-			if (kelime.liste[uzunluk][i]==girilenkelime[j] && i==j){
-				kosul3+=3;
-			}
-		}
-	}
-	
-	for (i=0; i<uzunluk1; i++){
-		for(j=0; j<uzunluk2; j++){
-			if (kelime.liste[uzunluk][i]==girilenkelime[j] && (i==j-1 || i==j+1)){
-				kosul2+=2;
-			}
-		}
-	}
-	
-	for(i=0; i<uzunluk1; i++){
-		for(j=0; j<uzunluk2; j++){
-			
-			if (kelime.liste[uzunluk][i]==girilenkelime[j] && (i==j-1 || i==j+1)){
-				continue;			
-			}
-			else if (kelime.liste[uzunluk][i]==girilenkelime[j] && i==j){
-				continue;
-			}
-			if (kelime.liste[uzunluk][i]==girilenkelime[j]){
-				kosul1++;
-			}
-		
-		}	
-	}
-	
-	toplampuan=kosul3+kosul2+kosul1;
-	
-	printf("%d.Indisin Benzerlik Puani: %d\n",indis,toplampuan);
-    indis++;
-	return toplampuan;
-}
-	
-void kelimeislemleri(){
-	
-	int i,j;
-	
-	 for(i=0;i<15;i++){
-    
-    		   printf("%d - %s \n",i+1,kelime.liste[i]);
-    		   
-			   
-			
-		}
-		printf("********************\n");
-	
-    
-    for(i=0;i<15;i++){
-    	for(j=0;j<15;j++){
-    		if(kelime.liste[i][j]=='a' || kelime.liste[i][j]=='e'|| kelime.liste[i][j]=='ý'|| kelime.liste[i][j]=='i'|| kelime.liste[i][j]=='u'|| kelime.liste[i][j]=='ü'|| kelime.liste[i][j]=='o'|| kelime.liste[i][j]=='ö'){
-    			kelime.sesli++;
-			}
-			
-		}
-		kelime.uzunluk=strlen(kelime.liste[i]);
-		kelime.sessiz=kelime.uzunluk-kelime.sesli;
-		printf("%d. dizi kelimesinin uzunlugu %d ve dizide %d tane sesli harf, %d tane sessiz harf vardir.\n",i+1,kelime.uzunluk,kelime.sesli,kelime.sessiz);
-		kelime.sessiz=0;
-		kelime.sesli=0;
-	}
-	printf("********************\n");
-	
-	
+    int i, j;
+    for (i = 0; i < 15; i++) {
+        if (word.list[length][i] == '\0') {
+            break;
+        }
+    }
+    int length1 = i;
+
+    for (i = 0; i < 15; i++) {
+        if (enteredWord[i] == '\0') {
+            break;
+        }
+    }
+    int length2 = i;
+
+    for (i = 0; i < length1; i++) {
+        for (j = 0; j < length2; j++) {
+            if (word.list[length][i] == enteredWord[j] && i == j) {
+                condition3 += 3;
+            }
+        }
+    }
+
+    for (i = 0; i < length1; i++) {
+        for (j = 0; j < length2; j++) {
+            if (word.list[length][i] == enteredWord[j] && (i == j - 1 || i == j + 1)) {
+                condition2 += 2;
+            }
+        }
+    }
+
+    for (i = 0; i < length1; i++) {
+        for (j = 0; j < length2; j++) {
+            if (word.list[length][i] == enteredWord[j] && (i == j - 1 || i == j + 1)) {
+                continue;
+            } else if (word.list[length][i] == enteredWord[j] && i == j) {
+                continue;
+            }
+            if (word.list[length][i] == enteredWord[j]) {
+                condition1++;
+            }
+        }
+    }
+
+    totalScore = condition3 + condition2 + condition1;
+
+    printf("%d. Index Similarity Score: %d\n", index, totalScore);
+    index++;
+    return totalScore;
 }
 
-void chardizisiatama(){
-	int sayac=0, kontrol=0;
-	for(sayac=0;sayac<15;sayac++){
-	
-			printf("%d. Dizi Elemani Icin En fazla 15 Kelimeden Olusan String Giriniz:",sayac+1);
-			scanf("%s",&kelime.liste[sayac]);
-		
-		
-	}
+void wordOperations() {
+    int i, j;
 
-	printf("********************\n");	
+    for (i = 0; i < 15; i++) {
+        printf("%d - %s \n", i + 1, word.list[i]);
+    }
+    printf("********************\n");
+
+    for (i = 0; i < 15; i++) {
+        for (j = 0; j < 15; j++) {
+            if (word.list[i][j] == 'a' || word.list[i][j] == 'e' || word.list[i][j] == 'i' || word.list[i][j] == 'o' ||
+                word.list[i][j] == 'u') {
+                word.vowels++;
+            }
+        }
+
+        word.length = strlen(word.list[i]);
+        word.consonants = word.length - word.vowels;
+
+        printf("%d. Word length is %d, and it has %d vowels and %d consonants.\n", i + 1, word.length, word.vowels,
+               word.consonants);
+
+        word.consonants = 0;
+        word.vowels = 0;
+    }
+    printf("********************\n");
 }
 
+void initializeCharArray() {
+    int counter = 0;
 
+    for (counter = 0; counter < 15; counter++) {
+        printf("Enter a string with at most 15 words for %d. Array Element:", counter + 1);
+        scanf("%s", word.list[counter]);
+    }
+
+    printf("********************\n");
+}
