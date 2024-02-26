97033ab9d1fead98ab34802c1781d6f2336a0892
Mehmet Yıldırım <65573335+erythropygia@users.noreply.github.com>
2024-02-26 16:59:25 +0300

    Update alternative_1.c

--- HomeworkandQuiz/alternative_1.c
+++ HomeworkandQuiz/alternative_1.c
@@ -1,120 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int dizidoldur(int min, int max, int *ptr, int sutun, int satir);
-int dizihesapla(int *ptr, int sutun, int satir);
-int satirtopla(int *ptr, int satir, int sutun,int satirkontrol);
+
+int fillArray(int min, int max, int *ptr, int columns, int rows);
+int calculateSecondLargest(int *ptr, int columns, int rows);
+int calculateRowSum(int *ptr, int rows, int columns, int targetRow);
 
 int main(){
-	int satir,sutun,satirkontrol;
-	srand(time(NULL));
-	printf("Lutfen Satir Sayisini Giriniz=");
-	scanf("%d",&satir);
-	printf("Lutfen Sutun Sayisini Giriniz=");
-	scanf("%d",&sutun);
-	int *ptr=calloc(satir*sutun,sizeof(int)); // dizimi tuttum. calloc ile tuttuðum için adresileri 0 ladý
-	int min,max;
-	kontrol:
-	printf("Lutfen Minimum Degeri Giriniz =");
-	scanf("%d",&min);
-	printf("Lutfen Maksimum Degeri Giriniz =");
-	scanf("%d",&max);
-	if(min>max){
-		printf("Hatali giris!! Lutfen Tekrar Deneyin \n");
-		goto kontrol;
-	}
-	dizidoldur(min,max,ptr,sutun,satir);
-	printf("\n\nDizinin en buyuk ikinci elemani = %d\n ",dizihesapla(ptr,sutun,satir));
-	printf("\n\n Lutfen Toplamak Istediginiz Satiri Giriniz= ");
-	scanf("%d",&satirkontrol);
-	printf("Girilen Satirin Toplami= %d",satirtopla(ptr,satir,sutun,satirkontrol));
-	free(ptr);	
+    int rows, columns, targetRow;
+    srand(time(NULL));
+    printf("Please enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Please enter the number of columns: ");
+    scanf("%d", &columns);
+    int *ptr = calloc(rows * columns, sizeof(int)); // I allocated memory for my array. I initialized with calloc, so the addresses are set to 0.
+    int min, max;
+
+    inputCheck:
+    printf("Please enter the minimum value: ");
+    scanf("%d", &min);
+    printf("Please enter the maximum value: ");
+    scanf("%d", &max);
+    if(min > max){
+        printf("Invalid input! Please try again.\n");
+        goto inputCheck;
+    }
+
+    fillArray(min, max, ptr, columns, rows);
+    printf("\n\nThe second largest element in the array is: %d\n ", calculateSecondLargest(ptr, columns, rows));
+    printf("\n\nPlease enter the row number you want to sum: ");
+    scanf("%d", &targetRow);
+    printf("The sum of the entered row is: %d", calculateRowSum(ptr, rows, columns, targetRow));
+    free(ptr);
+    return 0;
 }
 
-int dizidoldur(int min,int max, int *ptr, int sutun, int satir){
-	srand(time(0));
-    int i;
-    int kontrol;
-    
-    if(max-min<satir+sutun){
-    	printf("Bu Aralikta Birbirinden Farklý Random Uretmek Imkansizdir");
-    	return 0;
-	}
-       for(i=0;i<satir*sutun;i++){
-		ptr[i]=rand()%(max-min)+min;
-		for (kontrol=0;kontrol<i;kontrol++)
-                {
-                    if (ptr[kontrol]==ptr[i])
-                    {
-                        i--;
-                        break;
-                    }
-                    
-                }
+int fillArray(int min, int max, int *ptr, int columns, int rows){
+    srand(time(0));
+    int i, check;
+
+    if(max - min < rows + columns){
+        printf("It is impossible to generate distinct random numbers in this range.\n");
+        return 0;
+    }
+
+    for(i = 0; i < rows * columns; i++){
+        ptr[i] = rand() % (max - min) + min;
+        for (check = 0; check < i; check++){
+            if (ptr[check] == ptr[i]){
+                i--;
+                break;
             }
-	
-	
-	printf("Dizi: \n");
-	printf("**********");
-	
-	for(i=0;i<satir*sutun;i++){
-		if(i%sutun==0){
-			printf("\n");
-		}
-		printf("%d ",ptr[i]);
-	}
-}
-			
-
-
-int dizihesapla(int *ptr, int sutun, int satir){
-	int *max1=calloc(1,sizeof(int)); // 2. buyugu bulmamý saðlayacak dizileri tanýmlýyorum // static tanýmladým çünkü baþka bir fonksiyonda tekrar kullanmam gerek
-	int *max2=calloc(1,sizeof(int));
-	int i,j;
-	max1[0]=ptr[0];
-	max2[0]=ptr[1];
-	 for (i=1;i<satir*sutun;i++) 
-         if (ptr[i] > max1[0]) {
-            max2[0]= max1[0];
-            max1[0] = ptr[i];
-         }
-         else if (ptr[i] > max2[0]){
-         	 max2[0] = ptr[i];
         }
-	
-    return(max2[0]);
+    }
+
+    printf("Array: \n");
+    printf("**********");
+
+    for(i = 0; i < rows * columns; i++){
+        if(i % columns == 0){
+            printf("\n");
+        }
+        printf("%d ", ptr[i]);
+    }
+}
+
+int calculateSecondLargest(int *ptr, int columns, int rows){
+    int *max1 = calloc(1, sizeof(int)); // I'm defining the arrays that will allow me to find the second largest
+    int *max2 = calloc(1, sizeof(int));
+    int i, j;
+    max1[0] = ptr[0];
+    max2[0] = ptr[1];
+
+    for (i = 1; i < rows * columns; i++){
+        if (ptr[i] > max1[0]){
+            max2[0] = max1[0];
+            max1[0] = ptr[i];
+        } else if (ptr[i] > max2[0]){
+            max2[0] = ptr[i];
+        }
+    }
+
+    int result = max2[0];
     free(max1);
     free(max2);
- 
-   }
-   
-   
-int satirtopla(int *ptr, int satir, int sutun, int satirkontrol){
-	int i;
-	kontrol1:
-	if(satirkontrol>satir){
-		printf("\nHatali Giris Lutfen Tekrar Deneyin!");
-		goto kontrol1;
-	}
-	
-	int toplam=0;
-	satirkontrol=satirkontrol-1; // dizi hesaplamasý için azalttým yoksa dizi hesaplamasýnda problem yaþatýyor
-	for(i=0;i<sutun;i++){
-			toplam=toplam+*(ptr + satirkontrol*sutun + i);
-		}
-	   
-	return(toplam);
-
+    return result;
 }
 
+int calculateRowSum(int *ptr, int rows, int columns, int targetRow){
+    int i;
+    inputCheck1:
+    if(targetRow > rows){
+        printf("\nInvalid input! Please try again!");
+        goto inputCheck1;
+    }
 
-	
+    int sum = 0;
+    targetRow = targetRow - 1; // I reduced it for array calculation, otherwise it causes a problem in array calculation.
+    for(i = 0; i < columns; i++){
+        sum = sum + *(ptr + targetRow * columns + i);
+    }
 
-
-
-
-
-
-
-
-
+    return sum;
+}
